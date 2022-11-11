3b8c6d52fb21abeccbf0df50775966ac46f5f9e4
ColePriser <cpriser@purdue.edu>
2022-11-10 19:28:34 -0500

    Creating environment

--- VEX-Simulation.py
+++ VEX-Simulation.py
@@ -1,13 +1,19 @@
 import gym
-env = gym.make("LunarLander-v2", render_mode="human")
-env.action_space.seed(42)
-
-observation, info = env.reset(seed=42)
-
-for _ in range(1000):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
-    if terminated or truncated:
-        observation, info = env.reset()
-"temp"
-env.close()
+class BasicEnv(gym.Env):
+    def __init__(self):
+        self.action_space = gym.spaces.Discrete(5)
+        self.observation_space = gym.spaces.Discrete(2)
+    def step(self, action):
+        state = 1
+    
+        if action == 2:
+            reward = 1
+        else:
+            reward = -1
+            
+        done = True
+        info = {}
+        return state, reward, done, info
+    def reset(self):
+        state = 0
+        return state
